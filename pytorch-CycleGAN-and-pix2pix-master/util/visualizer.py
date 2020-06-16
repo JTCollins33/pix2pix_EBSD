79abe5b6312661d19819937d1a515017d0a05dcf
JTCollinsy99@gmail.com <64708923+JTCollins33@users.noreply.github.com>
2020-06-16 10:22:47 -0400

    Update visualizer.py

--- pytorch-CycleGAN-and-pix2pix-master/util/visualizer.py
+++ pytorch-CycleGAN-and-pix2pix-master/util/visualizer.py
@@ -159,6 +159,7 @@ class Visualizer():
                 for label, image in visuals.items():
                     image_numpy = util.tensor2im(image)
                     if (cnt == 1):
+                        print("\n\n\ndisplaying training results\n\n\n")
                         save_path_train_track = os.path.join(save_train_path, "image_1_epoch_"+str(epoch)+".png")
                         util.save_image(image_numpy, save_path_train_track)
                     label_html_row += '<td>%s</td>' % label
