3c5d39ecbf7d31ab3c8b973cc3d1f33239a2a816
JTCollinsy99@gmail.com <64708923+JTCollins33@users.noreply.github.com>
2020-06-18 13:14:45 -0400

    Update image_folder.py

--- pytorch-CycleGAN-and-pix2pix-master/data/image_folder.py
+++ pytorch-CycleGAN-and-pix2pix-master/data/image_folder.py
@@ -42,7 +42,7 @@ def mod_make_dataset(dir, max_dataset_size):
     #only get folders housing 36 images, not the actual images
     for i in range(1, max_dataset_size+1):
         if max_dataset_size <50:
-            i+=int(max_dataset_size*10)
+            i+=int(max_dataset_size*5)
         cDir = dir+"/ipf_image_"+str(i)
         if (os.path.isdir(cDir)):
             image_folders.append(cDir)
